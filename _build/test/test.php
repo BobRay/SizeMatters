bb3165de88ef87bef9cf1a316c0bf8e6b8eb13b4
bobray <bobray@softville.com>
2016-06-18 22:30:22 -0700

    Add fonts chart

--- _build/test/test.php
+++ _build/test/test.php
@@ -42,7 +42,7 @@ while (($line = fgetcsv($file)) !== FALSE) {
 }
 fclose($file);
 
-if (false) { /* ********************** */
+    /* ********************** */
     /*      Ems bar chart      */
     /* ********************** */
 
@@ -83,12 +83,56 @@ if (false) { /* ********************** */
 
     /* Render the picture (choose the best way) */
     $myPicture->autoOutput("ems-bar-chart.shaded.png");
-}
+
+
+
+    /* ********************** */
+    /*      Px bar chart      */
+    /* ********************** */
+    unset($MyData, $MyPicture, $pImage);
+
+    /* Create and populate the pData object */
+    $MyData = new pData();
+
+    /* Add main data */
+    $MyData->addPoints($pxs, "Width in CSS Pixels");
+    $MyData->setSerieDescription("Width CSS Pixels", "Width in CSS Pixels");
+
+    /* Set bar color */
+    $serieSettings = array("R" => 255, "G" => 0, "B" => 0, "Alpha" => 100);
+    $MyData->setPalette("Width in CSS Pixels", $serieSettings);
+
+    $MyData->setAxisName(0, "Hits");
+
+    /* Create the pChart object */
+    $myPicture = new pImage(900, 270, $MyData);
+    $myPicture->drawGradientArea(0, 0, 900, 270, DIRECTION_VERTICAL, array("StartR" => 0, "StartG" => 124, "StartB" => 180, "EndR" => 180, "EndG" => 180, "EndB" => 180, "Alpha" => 100));
+    $myPicture->drawGradientArea(0, 0, 900, 270, DIRECTION_HORIZONTAL, array("StartR" => 240, "StartG" => 240, "StartB" => 240, "EndR" => 180, "EndG" => 180, "EndB" => 180, "Alpha" => 20));
+    $myPicture->setFontProperties(array("FontName" => "C:/xampp/htdocs/addons/assets/mycomponents/sizematters/core/components/sizematters/model/pChart/fonts/verdana.ttf", "FontSize" => 10));
+
+
+    /* Draw the scale  */
+    $myPicture->setGraphArea(50, 30, 880, 200);
+    $myPicture->drawScale(array("CycleBackground" => TRUE, 'LabelRotation' => 90, "LabelSkip" => 49, "DrawSubTicks" => FALSE, "AutoAxisLabels" => FALSE, "GridR" => 0, "GridG" => 0, "GridB" => 0, "GridAlpha" => 10));
+    $myPicture->drawText(450, 55, "Viewport Width in CSS Pixels", array("FontSize" => 15, "Align" => TEXT_ALIGN_BOTTOMMIDDLE));
+    $myPicture->drawText(450, 250, "CSS Pixels", array("FontSize" => 10, "Align" => TEXT_ALIGN_BOTTOMMIDDLE));
+    /* Turn on shadow computing */
+    $myPicture->setShadow(TRUE, array("X" => 1, "Y" => 1, "R" => 0, "G" => 0, "B" => 0, "Alpha" => 10));
+
+    /* Draw the chart */
+    $settings = array("Gradient" => FALSE, "DisplayPos" => LABEL_POS_TOP, "DisplayValues" => FALSE, "DisplayR" => 0, "DisplayG" => 0, "DisplayB" => 0, "DisplayShadow" => FALSE,); //array("Surrounding"=>-30,"InnerSurrounding"=>30)
+    $myPicture->drawBarChart($settings);
+
+    /* Write the chart legend */
+// $myPicture->drawLegend(580, 12, array("Style" => LEGEND_BOX, "Mode" => LEGEND_HORIZONTAL));
+
+    /* Render the picture (choose the best way) */
+    $myPicture->autoOutput("pxs-bar-chart.shaded.png");
 
 
 
 /* ********************** */
-/*      Px bar chart      */
+/*      Font-size bar chart      */
 /* ********************** */
 unset($MyData, $MyPicture, $pImage);
 
@@ -96,17 +140,14 @@ unset($MyData, $MyPicture, $pImage);
 $MyData = new pData();
 
 /* Force Y axis to start at 0 */
-$MyData->addPoints(array(1), "Dummy");
-$serieSettings = array("R" => 255, "G" => 0, "B" => 0, "Alpha" => 0);
-$MyData->setPalette("Dummy", $serieSettings);
 
 /* Add main data */
-$MyData->addPoints($pxs, "Width in CSS Pixels");
-$MyData->setSerieDescription("Width CSS Pixels", "Width in CSS Pixels");
+$MyData->addPoints($fonts, "Font-size in Pixels");
+$MyData->setSerieDescription("Font-size in Pixels", "Width in CSS Pixels");
 
 /* Set bar color */
-$serieSettings = array("R" => 255, "G" => 0, "B" => 0, "Alpha" => 100);
-$MyData->setPalette("Width in CSS Pixels", $serieSettings);
+$serieSettings = array("R" => 255, "G" => 0, "B" => 0, "Alpha" => 70);
+$MyData->setPalette("Font-size in Pixels", $serieSettings);
 
 $MyData->setAxisName(0, "Hits");
 
@@ -119,9 +160,9 @@ $myPicture->setFontProperties(array("FontName" => "C:/xampp/htdocs/addons/assets
 
 /* Draw the scale  */
 $myPicture->setGraphArea(50, 30, 880, 200);
-$myPicture->drawScale(array("CycleBackground" => TRUE, 'LabelRotation' => 90, "LabelSkip" => 49, "DrawSubTicks" => FALSE, "AutoAxisLabels"=>FALSE, "GridR" => 0, "GridG" => 0, "GridB" => 0, "GridAlpha" => 10));
-$myPicture->drawText(450, 55, "Viewport Width in CSS Pixels", array("FontSize" => 15, "Align" => TEXT_ALIGN_BOTTOMMIDDLE));
-$myPicture->drawText(450, 250, "CSS Pixels", array("FontSize" => 10, "Align" => TEXT_ALIGN_BOTTOMMIDDLE));
+$myPicture->drawScale(array("CycleBackground" => TRUE, 'LabelRotation' => 90,  "AutoAxisLabels" => FALSE, "GridR" => 0, "GridG" => 0, "GridB" => 0, "GridAlpha" => 10));
+$myPicture->drawText(450, 55, "Font-size in Pixels", array("FontSize" => 15, "Align" => TEXT_ALIGN_BOTTOMMIDDLE));
+$myPicture->drawText(450, 250, "Pixels", array("FontSize" => 10, "Align" => TEXT_ALIGN_BOTTOMMIDDLE));
 /* Turn on shadow computing */
 $myPicture->setShadow(TRUE, array("X" => 1, "Y" => 1, "R" => 0, "G" => 0, "B" => 0, "Alpha" => 10));
 
@@ -133,7 +174,4 @@ $myPicture->drawBarChart($settings);
 // $myPicture->drawLegend(580, 12, array("Style" => LEGEND_BOX, "Mode" => LEGEND_HORIZONTAL));
 
 /* Render the picture (choose the best way) */
-$myPicture->autoOutput("pxs-bar-chart.shaded.png");
-
-
-
+$myPicture->autoOutput("fonts-bar-chart.shaded.png");
