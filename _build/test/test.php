2b98bee50018f4dd56a853cb986fc7f2174e0360
bobray <bobray@softville.com>
2016-06-18 20:43:21 -0700

    Wider image; label for X axis units

--- _build/test/test.php
+++ _build/test/test.php
@@ -47,17 +47,18 @@ $MyData->setAxisName(0, "Hits");
 
 
 /* Create the pChart object */
-$myPicture = new pImage(700, 230, $MyData);
-$myPicture->drawGradientArea(0, 0, 700, 230, DIRECTION_VERTICAL, array("StartR" => 0, "StartG" => 124, "StartB" => 180, "EndR" => 180, "EndG" => 180, "EndB" => 180, "Alpha" => 100));
-$myPicture->drawGradientArea(0, 0, 700, 230, DIRECTION_HORIZONTAL, array("StartR" => 240, "StartG" => 240, "StartB" => 240, "EndR" => 180, "EndG" => 180, "EndB" => 180, "Alpha" => 20));
+$myPicture = new pImage(900, 270, $MyData);
+$myPicture->drawGradientArea(0, 0, 900, 270, DIRECTION_VERTICAL, array("StartR" => 0, "StartG" => 124, "StartB" => 180, "EndR" => 180, "EndG" => 180, "EndB" => 180, "Alpha" => 100));
+$myPicture->drawGradientArea(0, 0, 900, 270, DIRECTION_HORIZONTAL, array("StartR" => 240, "StartG" => 240, "StartB" => 240, "EndR" => 180, "EndG" => 180, "EndB" => 180, "Alpha" => 20));
 $myPicture->setFontProperties(array("FontName" => "C:/xampp/htdocs/addons/assets/mycomponents/sizematters/core/components/sizematters/model/pChart/fonts/verdana.ttf", "FontSize" => 10));
 
 
 
 /* Draw the scale  */
-$myPicture->setGraphArea(50, 30, 680, 200);
+$myPicture->setGraphArea(50, 30, 880, 200);
 $myPicture->drawScale(array("CycleBackground" => TRUE, "LabelSkip" => 4, "LabelRotation" => 90, "DrawSubTicks" => TRUE, "GridR" => 0, "GridG" => 0, "GridB" => 0, "GridAlpha" => 10));
-$myPicture->drawText(350, 55, "Viewport Width in Ems", array("FontSize" => 15, "Align" => TEXT_ALIGN_BOTTOMMIDDLE));
+$myPicture->drawText(450, 55, "Viewport Width in Ems", array("FontSize" => 15, "Align" => TEXT_ALIGN_BOTTOMMIDDLE));
+$myPicture->drawText(450, 245, "Ems", array("FontSize" => 10, "Align" => TEXT_ALIGN_BOTTOMMIDDLE));
 /* Turn on shadow computing */
 $myPicture->setShadow(TRUE, array("X" => 1, "Y" => 1, "R" => 0, "G" => 0, "B" => 0, "Alpha" => 10));
 
@@ -71,6 +72,8 @@ $myPicture->drawBarChart($settings);
 /* Render the picture (choose the best way) */
 $myPicture->autoOutput("ems-bar-chart.shaded.png");
 
+
+exit;
 /* ********************** */
 /*      Px bar chart      */
 /* ********************** */
@@ -80,22 +83,18 @@ unset($MyData, $MyPicture, $pImage);
 $MyData = new pData();
 
 /* Force Y axis to start at 0 */
-// $pxs[0] = 1;
 $MyData->addPoints(array(1), "Dummy");
 $serieSettings = array("R" => 255, "G" => 0, "B" => 0, "Alpha" => 0);
 $MyData->setPalette("Dummy", $serieSettings);
 
+/* Add main data */
 $MyData->addPoints($pxs, "Width in CSS Pixels");
 $MyData->setSerieDescription("Width CSS Pixels", "Width in CSS Pixels");
 
-/* Draw serie 1 in red with a 80% opacity */
-
-$serieSettings = array("R" => 255, "G" => 0, "B" => 0, "Alpha" => 70);
+/* Set bar color */
+$serieSettings = array("R" => 255, "G" => 0, "B" => 0, "Alpha" => 100);
 $MyData->setPalette("Width in CSS Pixels", $serieSettings);
 
-// $MyData->setAbscissa("Width in Ems");
-// $MyData->addPoints(array(150, 220, 300, -250, -420, -200, 300, 200, 100), "Server A");
-// $MyData->addPoints(array(140, 0, 340, -300, -320, -300, 200, 100, 50), "Server B");
 $MyData->setAxisName(0, "Hits");
 
 /* Create the pChart object */
