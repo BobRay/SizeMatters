1d6490c5841386c180d09064aa881e5b07fb5bb3
bobray <bobray@softville.com>
2016-07-16 22:22:13 -0700

    Add pieConfigChunk property

---
+++ _build/data/properties/properties.sizemattersshowgraphs.snippet.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Properties file for SizeMattersShowGraphs snippet
+ *
+ * Copyright 2016 by Bob Ray <http://bobsguides.com>
+ * Created on 07-16-2016
+ *
+ * @package sizematters
+ * @subpackage build
+ */
+
+
+
+
+$properties = array (
+  'pieConfigChunk' => 
+  array (
+    'name' => 'pieConfigChunk',
+    'desc' => 'pie_config_chunk_desc',
+    'type' => 'textfield',
+    'options' => 
+    array (
+    ),
+    'value' => 'SizeMattersPieConfig',
+    'lexicon' => 'sizematters:default',
+    'area' => 'SizeMatters',
+  ),
+);
+
+return $properties;
+
